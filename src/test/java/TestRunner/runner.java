60c3b7dda7987c03acb155d129a1de136b84dd45
Sai Koteshara <Sai Koteshara@DESKTOP-RCN2N53.lan>
2023-04-18 12:13:11 +0530

    Using parameters argument

--- src/test/java/TestRunner/runner.java
+++ src/test/java/TestRunner/runner.java
@@ -4,14 +4,16 @@ package TestRunner;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class runner {
 	
+	@Parameters({"browserurl","print"})
 	@Test
-	public void Test() {
+	public void Test(String url,String path) {
 		
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("--remote-allow-origins=*");
@@ -19,8 +21,9 @@ public class runner {
 		WebDriver driver = new ChromeDriver(chromeOptions);
 		
 		//WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("https://www.airtel.in/recharge/prepaid/");
-		//System.out.println("abc");
+		driver.get(url);
+		//driver.get("https://www.airtel.in/recharge/prepaid/");
+		System.out.println(path);
 		driver.quit();
 	}
 }
